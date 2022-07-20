88f466dff2e4631b051a90e5e13d3309276c04ef
Nicolas Derobert <derobert.pro@gmail.com>
2022-07-20 15:59:24 +0200

    Aftere cleaning omments

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -8,11 +8,10 @@ import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import { ROUTES } from "../constants/routes";
-import Bills, { getBills } from "../containers/Bills.js";
+import Bills from "../containers/Bills.js";
 import store from "../__mocks__/store";
 import router from "../app/Router.js";
 
-// jest.mock("../containers/Bills.js", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
 	describe("When I am on Bills Page", () => {
@@ -33,99 +32,26 @@ describe("Given I am connected as an employee", () => {
 			window.onNavigate(ROUTES_PATH.Bills);
 			await waitFor(() => screen.getByTestId("icon-window"));
 			const windowIcon = screen.getByTestId("icon-window");
-			//to-do write expect expression
 			expect(windowIcon.classList.contains("active-icon")).toBe(true);
 		});
-		// test("The bills were get", async () => {
-		// 	Object.defineProperty(window, "localStorage", {
-		// 		value: localStorageMock,
-		// 	});
-		// 	window.localStorage.setItem(
-		// 		"user",
-		// 		JSON.stringify({
-		// 			type: "Employee",
-		// 		})
-		// 	);
-    //   const pathname = ROUTES_PATH['Bills'];
-		// 	const rootDiv = document.createElement("div");
-		// 	rootDiv.setAttribute("id", "root");
-		// 	document.body.append(root);
-		// 	router();
-		// 	// window.onNavigate(ROUTES_PATH.Bills);
-    //   rootDiv.innerHTML = ROUTES({ pathname, loading: true })
-    //   const divIcon1 = document.getElementById('layout-icon1')
-    //   const divIcon2 = document.getElementById('layout-icon2')
-    //   divIcon1.classList.add('active-icon')
-    //   divIcon2.classList.remove('active-icon')
-    //   const bills = new Bills({ document, onNavigate, store, localStorage  })
-    //   bills.getBills().then(data => {
-    //     rootDiv.innerHTML = BillsUI({ data })
-    //     const divIcon1 = document.getElementById('layout-icon1')
-    //     const divIcon2 = document.getElementById('layout-icon2')
-    //     divIcon1.classList.add('active-icon')
-    //     divIcon2.classList.remove('active-icon')
-    //     new Bills({ document, onNavigate, store, localStorage })
-    //   }).catch(error => {
-    //     rootDiv.innerHTML = ROUTES({ pathname, error })
-    //   })
-    //   console.log(root)
-    //   await waitFor(() => screen.getByText('Hôtel') )
-    //   // expect(screen.getByTestId(`open-billUIUZtnPQvnbFnB0ozvJh`)).toBeTruthy()
-    //   expect(screen.getByText('Hôtel')).toBeTruthy()
-		// 	// await waitFor(() => screen.getByTestId("icon-window"));
-		// 	// const windowIcon = screen.getByTestId("icon-window");
-		// 	// //to-do write expect expression
-		// 	// expect(windowIcon.classList.contains("active-icon")).toBe(true);
-
-		// });
+	
     test("The bills were get", async () => {
-		  // localStorage.setItem("user", JSON.stringify({ type: "Admin", email: "a@a" }));
    		localStorage.setItem("user", JSON.stringify({"type":"Employee","email":"employee@test.tld","password":"employee","status":"connected"}));
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
       router()
       window.onNavigate(ROUTES_PATH.Bills)
-
       const divIcon1 = document.getElementById('layout-icon1')
       const divIcon2 = document.getElementById('layout-icon2')
       divIcon1.classList.add('active-icon')
       divIcon2.classList.remove('active-icon')
       const bills = new Bills({ document, onNavigate, store, localStorage  })
-      // bills.getBills().then(data => {
-      //   rootDiv.innerHTML = BillsUI({ data })
-      //   const divIcon1 = document.getElementById('layout-icon1')
-      //   const divIcon2 = document.getElementById('layout-icon2')
-      //   divIcon1.classList.add('active-icon')
-      //   divIcon2.classList.remove('active-icon')
-      //   new Bills({ document, onNavigate, store, localStorage })
-      // }).catch(error => {
-      //   rootDiv.innerHTML = ROUTES({ pathname, error })
-      // })
       const getSpyBills = jest.spyOn(bills, "getBills");
       bills.getBills()
       expect(getSpyBills).toHaveBeenCalled();
-      // await waitFor(() => screen.getByText(' Mes notes de frais ') )
-      // expect(screen.getByText(' Mes notes de frais ')).toBeTruthy()
-
-      // await waitFor(() => screen.getByText("Validations"))
-      // const contentPending  = await screen.getByText("En attente (1)")
-      // expect(contentPending).toBeTruthy()
-      // const contentRefused  = await screen.getByText("Refusé (2)")
-      // expect(contentRefused).toBeTruthy()
-      // expect(screen.getByTestId("big-billed-icon")).toBeTruthy()
-		});
-		// test("Then bills should be ordered from earliest to latest", () => {
-		// 	document.body.innerHTML = BillsUI({ data: bills });
-		// 	const dates = screen
-		// 		.getAllByText(
-		// 			/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
-		// 		)
-		// 		.map((a) => a.innerHTML);
-		// 	const antiChrono = (a, b) => (a < b ? 1 : -1);
-		// 	const datesSorted = [...dates].sort(antiChrono);
-		// 	expect(dates).toEqual(datesSorted);
-		// });
+     		});
+		
     test("Then the bill page title should be displayed", () => {
       const html = BillsUI({ data: bills });
       document.body.innerHTML = html;
@@ -187,100 +113,3 @@ describe("Given I am connected as employee, and bill page is displayed", () => {
     });
   });
 });
-// describe("Given I am a user connected as Employee", () => {
-// 	describe("When I navigate to Bills", () => {
-// 	  test("fetches bills from mock API GET", async () => {
-// 		const getSpy = jest.spyOn(store, "list");
-// 		const bills = await store.get();
-// 		expect(getSpy).toHaveBeenCalledTimes(1);
-// 		expect(bills.data.length).toBe(4);
-// 	  });
-// 	  test("fetches bills from an API and fails with 404 message error", async () => {
-// 		store.get.mockImplementationOnce(() =>
-// 		  Promise.reject(new Error("Erreur 404"))
-// 		);
-// 		const html = BillsUI({ error: "Erreur 404" });
-// 		document.body.innerHTML = html;
-// 		const message = await screen.getByText(/Erreur 404/);
-// 		expect(message).toBeTruthy();
-// 	  });
-// 	  test("fetches messages from an API and fails with 500 message error", async () => {
-// 		store.get.mockImplementationOnce(() =>
-// 		  Promise.reject(new Error("Erreur 500"))
-// 		);
-// 		const html = BillsUI({ error: "Erreur 500" });
-// 		document.body.innerHTML = html;
-// 		const message = await screen.getByText(/Erreur 500/);
-// 		expect(message).toBeTruthy();
-// 	  });
-// 	});
-//   });
-
-//   describe("Given I am a user connected as Admin", () => {
-// 	describe("When I navigate to Dashboard", () => {
-// 	  test("fetches bills from mock API GET", async () => {
-// 		localStorage.setItem("user", JSON.stringify({"type":"Employee","email":"employee@test.tld","password":"employee","status":"connected"}));
-// 		// this.localStorage.setItem("user", JSON.stringify(user))
-// 		const root = document.createElement("div")
-// 		root.setAttribute("id", "root")
-// 		document.body.append(root)
-// 		router()
-// 		window.onNavigate(ROUTES_PATH.Bills)
-// 		console.log(screen)
-// 		await waitFor(() => screen.getByText("Mes notes de frais"))
-// 		// const contentPending  = await screen.getByText("En attente (1)")
-// 		// expect(contentPending).toBeTruthy()
-// 		// const contentRefused  = await screen.getByText("Refusé (2)")
-// 		// expect(contentRefused).toBeTruthy()
-// 		// expect(screen.getByTestId("big-billed-icon")).toBeTruthy()
-// 	  })
-// 	describe("When an error occurs on API", () => {
-// 	  beforeEach(() => {
-// 		jest.spyOn(mockStore, "bills")
-// 		Object.defineProperty(
-// 			window,
-// 			'localStorage',
-// 			{ value: localStorageMock }
-// 		)
-// 		window.localStorage.setItem('user', JSON.stringify({
-// 		  type: 'Admin',
-// 		  email: "a@a"
-// 		}))
-// 		const root = document.createElement("div")
-// 		root.setAttribute("id", "root")
-// 		document.body.appendChild(root)
-// 		router()
-// 	  })
-// 	  test("fetches bills from an API and fails with 404 message error", async () => {
-  
-// 		mockStore.bills.mockImplementationOnce(() => {
-// 		  return {
-// 			list : () =>  {
-// 			  return Promise.reject(new Error("Erreur 404"))
-// 			}
-// 		  }})
-// 		window.onNavigate(ROUTES_PATH.Dashboard)
-// 		await new Promise(process.nextTick);
-// 		const message = await screen.getByText(/Erreur 404/)
-// 		expect(message).toBeTruthy()
-// 	  })
-  
-// 	  test("fetches messages from an API and fails with 500 message error", async () => {
-  
-// 		mockStore.bills.mockImplementationOnce(() => {
-// 		  return {
-// 			list : () =>  {
-// 			  return Promise.reject(new Error("Erreur 500"))
-// 			}
-// 		  }})
-  
-// 		window.onNavigate(ROUTES_PATH.Dashboard)
-// 		await new Promise(process.nextTick);
-// 		const message = await screen.getByText(/Erreur 500/)
-// 		expect(message).toBeTruthy()
-// 	  })
-// 	})
-  
-// 	})
-//   })
-  
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -146,24 +146,6 @@ export default class {
 	};
 
 	handleShowTickets(e, bills, index) {
-		// if (this.counter === undefined || this.index !== index) this.counter = 0;
-		// if (this.counter === undefined) this.counter = 0;
-		// if (index === 1) {
-		// 	if (this.counter1 === undefined || this.index !== index)
-		// 		this.counter1 = 0;
-		// 	if (this.index === undefined || this.index !== index) this.index = index;
-		// 	if (this.counter1 % 2 === 0) {
-		// 		$(`#arrow-icon${this.index}`).css({ transform: "rotate(0deg)" });
-		// 		$(`#status-bills-container${this.index}`).html(
-		// 			cards(filteredBills(bills, getStatus(this.index)))
-		// 		);
-		// 		this.counter1++;
-		// 	} else {
-		// 		$(`#arrow-icon${this.index}`).css({ transform: "rotate(90deg)" });
-		// 		$(`#status-bills-container${this.index}`).html("");
-		// 		this.counter1++;
-		// 	}
-		// }
 		if (index === 1) {
 			if (this.counter1 === undefined )
 				this.counter1 = 0;
